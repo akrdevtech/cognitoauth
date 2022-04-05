6caf475c0230181188f2e0b8890ac05ebdd8609e
Akhil Krishnan <94969469+akrdevtech@users.noreply.github.com>
2022-04-05 18:05:06 +0530

    id and refresh tokens included

--- services/authservice.js
+++ services/authservice.js
@@ -3,47 +3,52 @@ require('dotenv').config();
 
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 const poolData = {
-   UserPoolId: process.env.USER_POOL_ID,
-   ClientId: process.env.CLIENT_ID,
+    UserPoolId: process.env.USER_POOL_ID,
+    ClientId: process.env.CLIENT_ID,
 };
 const pool_region = 'ca-central-1';
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 exports.Register = function (body, callback) {
-   var name = body.name;
-   var email = body.email;
-   var password = body.password;
-   var attributeList = [];
-   
-   attributeList.push(new    AmazonCognitoIdentity.CognitoUserAttribute({ Name: "email", Value: email }));
-   userPool.signUp(name, password, attributeList, null, function (err, result) {
-     if (err)
-         callback(err);
-     var cognitoUser = result.user;
-     callback(null, cognitoUser);
-   })
+    var name = body.name;
+    var email = body.email;
+    var password = body.password;
+    var attributeList = [];
+
+    attributeList.push(new AmazonCognitoIdentity.CognitoUserAttribute({ Name: "email", Value: email }));
+    userPool.signUp(name, password, attributeList, null, function (err, result) {
+        if (err)
+            callback(err);
+        var cognitoUser = result.user;
+        callback(null, cognitoUser);
+    })
 }
 
 exports.Login = function (body, callback) {
     var userName = body.name;
     var password = body.password;
     var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
-         Username: userName,
-         Password: password
-     });
-     var userData = {
-         Username: userName,
-         Pool: userPool
-     }
-     var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-     cognitoUser.authenticateUser(authenticationDetails, {
-         onSuccess: function (result) {
-            var accesstoken = result.getAccessToken().getJwtToken();
-            callback(null, accesstoken);
-         },
-         onFailure: (function (err) {
+        Username: userName,
+        Password: password
+    });
+    var userData = {
+        Username: userName,
+        Pool: userPool
+    }
+    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function (result) {
+            var accessToken = result.getAccessToken().getJwtToken();
+            var idToken = result.getIdToken().getJwtToken();
+            var refreshToken = result.getRefreshToken().getJwtToken();
+            callback(null, {
+                accessToken,
+                idToken,
+                refreshToken
+            });
+        },
+        onFailure: (function (err) {
             callback(err);
         })
     })
- };
+};
 
- 
